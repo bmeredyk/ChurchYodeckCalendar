8e7abb7ea0d3aa1c7d24500f5f8299734e91afd2
Brian Meredyk <bmeredyk@gmail.com>
2025-10-05 08:27:23 -0500

    update vite for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/website_calendar/', // Set to your repo name for GitHub Pages
 })
