2b76206a43306b6e4f1c0a6d55783d62209993ef
Brian Meredyk <bmeredyk@gmail.com>
2025-10-05 08:57:22 -0500

    trying to get gh pages working

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/website_calendar/', // Set to your repo name for GitHub Pages
+  base: '/ChurchYodeckCalendar/', // Set to your repo name for GitHub Pages
 })
